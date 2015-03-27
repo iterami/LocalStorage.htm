c78b68973a194162f7b73a4ab46da5f5603ba370
Jan Horava <czechinthemail@gmail.com>
2015-03-27 13:35:48 -0700

    added refresh() function

--- js/localstorage.js
+++ js/localstorage.js
@@ -9,9 +9,24 @@ function clearAll(){
 function headers(){
     document.getElementById('result').innerHTML =
       '<tr class=top>'
-        + '<td>Key <input onclick=removeAll() type=button value=removeAll()><input onclick=clearAll() type=button value=window.localStorage.clear()>'
+        + '<td>Key <input onclick=refresh() type=button value=Refresh><input onclick=removeAll() type=button value=removeAll()><input onclick=clearAll() type=button value=window.localStorage.clear()>'
         + '<td>Value'
       + document.getElementById('result').innerHTML;
+}
+
+function refresh(){
+    var output = '';
+
+    for(var key in window.localStorage){
+        output += '<tr><td id="' + key + '">'
+          + key
+          + ' <input onclick="removeItem(\'' + key + '\', this)" type=button value=X><td>'
+          + window.localStorage.getItem(key);
+    }
+
+    document.getElementById('result').innerHTML = output;
+
+    headers();
 }
 
 function removeAll(){
@@ -36,17 +51,4 @@ function removeItem(key, element){
     element.parentElement.parentElement.remove();
 }
 
-window.onload = function(e){
-    var output = '';
-
-    for(var key in window.localStorage){
-        output += '<tr><td id="' + key + '">'
-          + key
-          + ' <input onclick="removeItem(\'' + key + '\', this)" type=button value=X><td>'
-          + window.localStorage.getItem(key);
-    }
-
-    document.getElementById('result').innerHTML = output;
-
-    headers();
-};
+window.onload = refresh;
