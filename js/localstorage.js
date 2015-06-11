170b3b33f048cf96bdd2d36e3dec582f10a80c3a
Jan Horava <czechinthemail@gmail.com>
2015-06-10 19:32:02 -0700

    added removeChecked(), added refresh() to clearAll()

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,15 +1,21 @@
+function check_checkbox(id){
+    document.getElementById('checkbox-' + id).checked =
+      !document.getElementById('checkbox-' + id).checked;
+}
+
 function clearAll(){
     if(!window.confirm('window.localStorage.clear()?')){
         return;
     }
 
     window.localStorage.clear();
+    refresh();
 }
 
 function headers(){
     document.getElementById('result').innerHTML =
       '<tr class=top>'
-        + '<td>Key <input onclick=refresh() type=button value="Refresh [R]"><input onclick=removeAll() type=button value=removeAll()><input onclick=clearAll() type=button value=window.localStorage.clear()>'
+        + '<td>Key <input onclick=refresh() type=button value="Refresh [R]"><input onclick=removeAll() type=button value=removeAll()><input onclick=removeChecked() type=button value=removeChecked()><input onclick=clearAll() type=button value=window.localStorage.clear()>'
         + '<td>Value'
       + document.getElementById('result').innerHTML;
 }
@@ -18,9 +24,13 @@ function refresh(){
     var output = '';
 
     for(var key in window.localStorage){
-        output += '<tr><td id="' + key + '">'
+        output += '<tr onclick="check_checkbox(\''
           + key
-          + ' <input onclick="removeItem(\'' + key + '\', this)" type=button value=X><td>'
+          + '\')"><td id="' + key + '"><input id="checkbox-'
+          + key
+          + '" type=checkbox><input onclick="removeItem(\'' + key + '\', this)" type=button value=X> '
+          + key
+          + '<td>'
           + window.localStorage.getItem(key);
     }
 
@@ -40,6 +50,22 @@ function removeAll(){
 
     document.getElementById('result').innerHTML = '';
     headers();
+}
+
+function removeChecked(){
+    if(!window.confirm('removeChecked()?')){
+        return;
+    }
+
+    for(var key in window.localStorage){
+        if(!document.getElementById('checkbox-' + key).checked){
+            continue;
+        }
+
+        window.localStorage.removeItem(key);
+    }
+
+    refresh();
 }
 
 function removeItem(key, element){
